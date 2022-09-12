55eee58fce2b70c20257af5471a05a533030f802
Data-Nexus <derek.m.meyer@gmail.com>
2022-09-12 13:34:58 -0400

    Fixed Timezone Change for unexpected null

--- src/mappings/rocketNetworkBalancesMapping.ts
+++ src/mappings/rocketNetworkBalancesMapping.ts
@@ -1,133 +1,110 @@
-import { BalancesUpdated } from '../../generated/rocketNetworkBalances/rocketNetworkBalances'
-import { rocketTokenRETH } from '../../generated/rocketNetworkBalances/rocketTokenRETH'
-import { rocketDepositPool } from '../../generated/rocketNetworkBalances/rocketDepositPool'
-import {
-  Staker,
-  NetworkStakerBalanceCheckpoint,
-  RocketPoolProtocol,
-} from '../../generated/schema'
-import { generalUtilities } from '../utilities/generalUtilities'
-import { stakerUtilities } from '../utilities/stakerutilities'
-import { rocketPoolEntityFactory } from '../entityfactory'
+import { BalancesUpdated } from "../../generated/rocketNetworkBalances/rocketNetworkBalances";
+import { rocketTokenRETH } from "../../generated/rocketNetworkBalances/rocketTokenRETH";
+import { rocketDepositPool } from "../../generated/rocketNetworkBalances/rocketDepositPool";
+import { Staker, NetworkStakerBalanceCheckpoint, RocketPoolProtocol } from "../../generated/schema";
+import { generalUtilities } from "../utilities/generalUtilities";
+import { stakerUtilities } from "../utilities/stakerutilities";
+import { rocketPoolEntityFactory } from "../entityfactory";
 import {
   ZERO_ADDRESS_STRING,
   ROCKET_DEPOSIT_POOL_CONTRACT_ADDRESS,
   ROCKET_TOKEN_RETH_CONTRACT_ADDRESS,
-} from './../constants/contractconstants'
-import { Address, BigInt } from '@graphprotocol/graph-ts'
+} from "./../constants/contractconstants";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 
 /**
  * When enough ODAO members votes on a balance and a consensus threshold is reached, the staker beacon chain state is persisted to the smart contracts.
  */
 export function handleBalancesUpdated(event: BalancesUpdated): void {
   // Protocol entity should exist, if not, then we attempt to create it.
-  let protocol = generalUtilities.getRocketPoolProtocolEntity()
+  let protocol = generalUtilities.getRocketPoolProtocolEntity();
   if (protocol === null || protocol.id == null) {
-    protocol = rocketPoolEntityFactory.createRocketPoolProtocol()
+    protocol = rocketPoolEntityFactory.createRocketPoolProtocol();
   }
-  if (protocol === null) return
+  if (protocol === null) return;
 
   // Preliminary check to ensure we haven't handled this before.
-  if (
-    stakerUtilities.hasNetworkStakerBalanceCheckpointHasBeenIndexed(
-      protocol,
-      event,
-    )
-  )
-    return
+  if (stakerUtilities.hasNetworkStakerBalanceCheckpointHasBeenIndexed(protocol, event)) return;
 
   // Load the RocketTokenRETH contract
   // We will need the rocketvault smart contract state to get specific addresses.
-  let rETHContract = rocketTokenRETH.bind(
-    Address.fromString(ROCKET_TOKEN_RETH_CONTRACT_ADDRESS),
-  )
-  if (rETHContract === null) return
+  let rETHContract = rocketTokenRETH.bind(Address.fromString(ROCKET_TOKEN_RETH_CONTRACT_ADDRESS));
+  if (rETHContract === null) return;
 
   // Load the rocketDepositPool contract
-  let rocketDepositPoolContract = rocketDepositPool.bind(
-    Address.fromString(ROCKET_DEPOSIT_POOL_CONTRACT_ADDRESS),
-  )
-  if (rocketDepositPoolContract === null) return
+  let rocketDepositPoolContract = rocketDepositPool.bind(Address.fromString(ROCKET_DEPOSIT_POOL_CONTRACT_ADDRESS));
+  if (rocketDepositPoolContract === null) return;
 
   // How much is the total staker ETH balance in the deposit pool?
-  let depositPoolBalance = rocketDepositPoolContract.getBalance()
-  let depositPoolExcessBalance = rocketDepositPoolContract.getExcessBalance()
+  let depositPoolBalance = rocketDepositPoolContract.getBalance();
+  let depositPoolExcessBalance = rocketDepositPoolContract.getExcessBalance();
 
   // The RocketEth contract balance is equal to the total collateral - the excess deposit pool balance.
-  let stakerETHInRocketETHContract = getRocketETHBalance(
-    depositPoolExcessBalance,
-    rETHContract.getTotalCollateral(),
-  )
+  let stakerETHInRocketETHContract = getRocketETHBalance(depositPoolExcessBalance, rETHContract.getTotalCollateral());
 
   // Attempt to create a new network balance checkpoint.
-  let rETHExchangeRate = rETHContract.getExchangeRate()
+  let rETHExchangeRate = rETHContract.getExchangeRate();
   let checkpoint = rocketPoolEntityFactory.createNetworkStakerBalanceCheckpoint(
     generalUtilities.extractIdForEntity(event),
     protocol.lastNetworkStakerBalanceCheckPoint,
     event,
     depositPoolBalance,
     stakerETHInRocketETHContract,
-    rETHExchangeRate,
-  )
-  if (checkpoint === null) return
+    rETHExchangeRate
+  );
+  if (checkpoint === null) return;
 
   // Retrieve previous checkpoint.
-  let previousCheckpointId = protocol.lastNetworkStakerBalanceCheckPoint
-  let previousTotalStakerETHRewards = BigInt.fromI32(0)
-  let previousTotalStakersWithETHRewards = BigInt.fromI32(0)
-  let previousRETHExchangeRate = BigInt.fromI32(1)
-  let previousCheckpoint: NetworkStakerBalanceCheckpoint | null = null
-  if (previousCheckpointId != null) {
-    previousCheckpoint = NetworkStakerBalanceCheckpoint.load(
-      <string>previousCheckpointId,
-    )
-    if (previousCheckpoint !== null) {
-      previousTotalStakerETHRewards = previousCheckpoint.totalStakerETHRewards
-      previousTotalStakersWithETHRewards =
-        previousCheckpoint.totalStakersWithETHRewards
-      previousRETHExchangeRate = previousCheckpoint.rETHExchangeRate
-      previousCheckpoint.nextCheckpointId = checkpoint.id
+  let previousCheckpointId = protocol.lastNetworkStakerBalanceCheckPoint;
+  let previousTotalStakerETHRewards = BigInt.fromI32(0);
+  let previousTotalStakersWithETHRewards = BigInt.fromI32(0);
+  let previousRETHExchangeRate = BigInt.fromI32(1);
+  let previousCheckpoint: NetworkStakerBalanceCheckpoint | null = null;
+  if (previousCheckpointId) {
+    previousCheckpoint = NetworkStakerBalanceCheckpoint.load(previousCheckpointId);
+    if (previousCheckpoint) {
+      previousTotalStakerETHRewards = previousCheckpoint.totalStakerETHRewards;
+      previousTotalStakersWithETHRewards = previousCheckpoint.totalStakersWithETHRewards;
+      previousRETHExchangeRate = previousCheckpoint.rETHExchangeRate;
+      previousCheckpoint.nextCheckpointId = checkpoint.id;
     }
   }
 
   // Handle the staker impact.
   generateStakerBalanceCheckpoints(
     protocol.activeStakers,
-    <NetworkStakerBalanceCheckpoint>checkpoint,
-    previousCheckpoint !== null
-      ? <NetworkStakerBalanceCheckpoint>previousCheckpoint
-      : null,
+    checkpoint,
+    previousCheckpoint !== null ? previousCheckpoint : null,
     previousRETHExchangeRate,
     event.block.number,
     event.block.timestamp,
-    protocol,
-  )
+    protocol
+  );
 
   // If for some reason the running summary totals up to this checkpoint was 0, then we try to set it based on the previous checkpoint.
   if (checkpoint.totalStakerETHRewards == BigInt.fromI32(0)) {
-    checkpoint.totalStakerETHRewards = previousTotalStakerETHRewards
+    checkpoint.totalStakerETHRewards = previousTotalStakerETHRewards;
   }
   if (checkpoint.totalStakersWithETHRewards == BigInt.fromI32(0)) {
-    checkpoint.totalStakersWithETHRewards = previousTotalStakersWithETHRewards
+    checkpoint.totalStakersWithETHRewards = previousTotalStakersWithETHRewards;
   }
 
   // Calculate average staker reward up to this checkpoint.
   if (
-    checkpoint.totalStakerETHRewards != BigInt.fromI32(0) &&
-    checkpoint.totalStakersWithETHRewards >= BigInt.fromI32(1)
+    checkpoint.totalStakerETHRewards != BigInt.fromI32(0) && checkpoint.totalStakersWithETHRewards
+      ? 0
+      : checkpoint.totalStakersWithETHRewards >= BigInt.fromI32(1)
   ) {
-    checkpoint.averageStakerETHRewards = checkpoint.totalStakerETHRewards.div(
-      checkpoint.totalStakersWithETHRewards,
-    )
+    checkpoint.averageStakerETHRewards = checkpoint.totalStakerETHRewards.div(checkpoint.totalStakersWithETHRewards);
   }
 
   // Update the link so the protocol points to the last network staker balance checkpoint.
-  protocol.lastNetworkStakerBalanceCheckPoint = checkpoint.id
+  protocol.lastNetworkStakerBalanceCheckPoint = checkpoint.id;
 
   // Index these changes.
-  checkpoint.save()
-  if (previousCheckpoint !== null) previousCheckpoint.save()
-  protocol.save()
+  checkpoint.save();
+  if (previousCheckpoint !== null) previousCheckpoint.save();
+  protocol.save();
 }
 
 /**
@@ -142,33 +119,26 @@ function generateStakerBalanceCheckpoints(
   previousRETHExchangeRate: BigInt,
   blockNumber: BigInt,
   blockTime: BigInt,
-  protocol: RocketPoolProtocol,
+  protocol: RocketPoolProtocol
 ): void {
   // Update grand totals based on previous checkpoint before we do anything.
-  stakerUtilities.updateNetworkStakerBalanceCheckpointForPreviousCheckpointAndProtocol(
-    networkCheckpoint,
-    previousCheckpoint,
-    protocol,
-  )
+  stakerUtilities.updateNetworkStakerBalanceCheckpointForPreviousCheckpointAndProtocol(networkCheckpoint, previousCheckpoint, protocol);
 
   // Loop through all the staker id's in the protocol.
   for (let index = 0; index < activeStakerIds.length; index++) {
     // Determine current staker ID.
-    let stakerId = <string>activeStakerIds[index]
-    if (stakerId == null || stakerId == ZERO_ADDRESS_STRING) continue
+    let stakerId = <string>activeStakerIds[index];
+    if (stakerId == null || stakerId == ZERO_ADDRESS_STRING) continue;
 
     // Load the indexed staker.
-    let staker = Staker.load(stakerId)
+    let staker = Staker.load(stakerId);
 
     // Shouldn't occur since we're only passing in staker ID's that have an active rETH balance.
-    if (staker === null || staker.rETHBalance == BigInt.fromI32(0)) continue
+    if (staker === null || staker.rETHBalance == BigInt.fromI32(0)) continue;
 
     // Get the current & previous balances for this staker and update the staker balance for the current exchange rate.
-    let stakerBalance = stakerUtilities.getStakerBalance(
-      <Staker>staker,
-      networkCheckpoint.rETHExchangeRate,
-    )
-    staker.ethBalance = stakerBalance.currentETHBalance
+    let stakerBalance = stakerUtilities.getStakerBalance(<Staker>staker, networkCheckpoint.rETHExchangeRate);
+    staker.ethBalance = stakerBalance.currentETHBalance;
 
     // Calculate rewards (+/-) for this staker since the previous checkpoint.
     let ethRewardsSincePreviousCheckpoint = stakerUtilities.getETHRewardsSincePreviousStakerBalanceCheckpoint(
@@ -177,48 +147,37 @@ function generateStakerBalanceCheckpoints(
       stakerBalance.previousRETHBalance,
       stakerBalance.previousETHBalance,
       previousRETHExchangeRate,
-      networkCheckpoint.rETHExchangeRate,
-    )
-    stakerUtilities.handleEthRewardsSincePreviousCheckpoint(
-      ethRewardsSincePreviousCheckpoint,
-      <Staker>staker,
-      networkCheckpoint,
-      protocol,
-    )
+      networkCheckpoint.rETHExchangeRate
+    );
+    stakerUtilities.handleEthRewardsSincePreviousCheckpoint(ethRewardsSincePreviousCheckpoint, <Staker>staker, networkCheckpoint, protocol);
 
     // Create a new staker balance checkpoint
     let stakerBalanceCheckpoint = rocketPoolEntityFactory.createStakerBalanceCheckpoint(
-      networkCheckpoint.id + ' - ' + stakerId,
+      networkCheckpoint.id + " - " + stakerId,
       staker,
       networkCheckpoint,
       stakerBalance.currentETHBalance,
       stakerBalance.currentRETHBalance,
       staker.totalETHRewards,
       blockNumber,
-      blockTime,
-    )
-    if (stakerBalanceCheckpoint == null) continue
-    staker.lastBalanceCheckpoint = stakerBalanceCheckpoint.id
+      blockTime
+    );
+    if (stakerBalanceCheckpoint == null) continue;
+    staker.lastBalanceCheckpoint = stakerBalanceCheckpoint.id;
 
     // Index both the updated staker & the new staker balance checkpoint.
-    stakerBalanceCheckpoint.save()
-    staker.save()
+    stakerBalanceCheckpoint.save();
+    staker.save();
   }
 }
 
 /**
  * The RocketETH contract balance is equal to the total collateral - the excess deposit pool balance.
  */
-function getRocketETHBalance(
-  depositPoolExcess: BigInt,
-  rocketETHTotalCollateral: BigInt,
-): BigInt {
-  let totalStakerETHInRocketEthContract = rocketETHTotalCollateral.minus(
-    depositPoolExcess,
-  )
+function getRocketETHBalance(depositPoolExcess: BigInt, rocketETHTotalCollateral: BigInt): BigInt {
+  let totalStakerETHInRocketEthContract = rocketETHTotalCollateral.minus(depositPoolExcess);
 
-  if (totalStakerETHInRocketEthContract < BigInt.fromI32(0))
-    totalStakerETHInRocketEthContract = BigInt.fromI32(0)
+  if (totalStakerETHInRocketEthContract < BigInt.fromI32(0)) totalStakerETHInRocketEthContract = BigInt.fromI32(0);
 
-  return totalStakerETHInRocketEthContract
+  return totalStakerETHInRocketEthContract;
 }
--- src/mappings/rocketNodeManager.ts
+++ src/mappings/rocketNodeManager.ts
@@ -1,133 +1,101 @@
-import { BigInt, Address } from '@graphprotocol/graph-ts'
-import { generalUtilities } from '../utilities/generalutilities'
-import { rocketPoolEntityFactory } from '../entityfactory'
-import { Node, NetworkNodeTimezone } from '../../generated/schema'
-import {
-  rocketNodeManager,
-  NodeRegistered,
-  NodeTimezoneLocationSet,
-} from '../../generated/rocketNodeManager/rocketNodeManager'
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { generalUtilities } from "../utilities/generalutilities";
+import { rocketPoolEntityFactory } from "../entityfactory";
+import { Node, NetworkNodeTimezone } from "../../generated/schema";
+import { rocketNodeManager, NodeRegistered, NodeTimezoneLocationSet } from "../../generated/rocketNodeManager/rocketNodeManager";
 
 /**
  * Occurs when a node operator registers his address with the RocketPool protocol.
  */
 export function handleNodeRegister(event: NodeRegistered): void {
   // Preliminary null checks.
-  if (event === null || event.params === null || event.params.node === null)
-    return
+  if (event === null || event.params === null || event.params.node === null) return;
 
   // We can only register an address as a node if it hasn't been registered yet.
-  let node = Node.load(event.params.node.toHex())
-  if (node !== null) return
+  let node = Node.load(event.params.node.toHex());
+  if (node !== null) return;
 
   // Retrieve the associated timezone, if the timezone doesn't exist yet, we need to create it.
-  let nodeTimezoneStringId = getNodeTimezoneId(
-    event.params.node.toHex(),
-    event.address,
-  )
-  let nodeTimezone = NetworkNodeTimezone.load(nodeTimezoneStringId)
+  let nodeTimezoneStringId = getNodeTimezoneId(event.params.node.toHex(), event.address);
+  let nodeTimezone = NetworkNodeTimezone.load(nodeTimezoneStringId);
   if (nodeTimezone === null || nodeTimezone.id == null) {
-    nodeTimezone = rocketPoolEntityFactory.createNodeTimezone(
-      nodeTimezoneStringId,
-    )
+    nodeTimezone = rocketPoolEntityFactory.createNodeTimezone(nodeTimezoneStringId);
+    nodeTimezone.block = event.block.number;
+    nodeTimezone.blockTime = event.block.timestamp;
   }
 
   // Increment the total registered nodes for this timezone.
-  nodeTimezone.totalRegisteredNodes = nodeTimezone.totalRegisteredNodes.plus(
-    BigInt.fromI32(1),
-  )
+  nodeTimezone.totalRegisteredNodes = nodeTimezone.totalRegisteredNodes.plus(BigInt.fromI32(1));
 
   // Create the node for this timezone and index it.
-  node = rocketPoolEntityFactory.createNode(
-    event.params.node.toHexString(),
-    nodeTimezone.id,
-    event.block.number,
-    event.block.timestamp,
-  )
-  if (node === null) return
+  node = rocketPoolEntityFactory.createNode(event.params.node.toHexString(), nodeTimezone.id, event.block.number, event.block.timestamp);
+  if (node === null) return;
 
   // Protocol entity should exist, if not, then we attempt to create it.
-  let protocol = generalUtilities.getRocketPoolProtocolEntity()
+  let protocol = generalUtilities.getRocketPoolProtocolEntity();
   if (protocol === null || protocol.id == null) {
-    protocol = rocketPoolEntityFactory.createRocketPoolProtocol()
+    protocol = rocketPoolEntityFactory.createRocketPoolProtocol();
   }
 
   // Add this node to the collection of the protocol if necessary and index.
-  let protocolNodes = protocol.nodes
-  if (protocol.nodes.indexOf(node.id) == -1) protocolNodes.push(node.id)
-  protocol.nodes = protocolNodes
+  let protocolNodes = protocol.nodes;
+  if (protocol.nodes.indexOf(node.id) == -1) protocolNodes.push(node.id);
+  protocol.nodes = protocolNodes;
 
   // Index changes.
-  node.save()
-  nodeTimezone.save()
-  protocol.save()
+  node.save();
+  nodeTimezone.save();
+  protocol.save();
 }
 
 /**
  * Occurs when a node operator changes his timzone in the RocketPool protocol.
  */
-export function handleNodeTimezoneChanged(
-  event: NodeTimezoneLocationSet,
-): void {
-  if (event === null || event.params === null || event.params.node === null)
-    return
+export function handleNodeTimezoneChanged(event: NodeTimezoneLocationSet): void {
+  if (event === null || event.params === null || event.params.node === null) return;
 
   // The node in question has to exist before we can change its timezone.
-  let node = Node.load(event.params.node.toHexString())
-  if (node === null) return
+  let node = Node.load(event.params.node.toHexString());
+  if (node === null) return;
 
-  let oldNodeTimezone: NetworkNodeTimezone | null = null
+  let oldNodeTimezone: NetworkNodeTimezone | null = null;
 
   // Decrement the total registered nodes for the old timezone.
   if (node.timezone != null) {
-    oldNodeTimezone = NetworkNodeTimezone.load(node.timezone)
+    oldNodeTimezone = NetworkNodeTimezone.load(node.timezone);
     if (oldNodeTimezone !== null) {
-      oldNodeTimezone.totalRegisteredNodes = oldNodeTimezone.totalRegisteredNodes.minus(
-        BigInt.fromI32(1),
-      )
+      oldNodeTimezone.totalRegisteredNodes = oldNodeTimezone.totalRegisteredNodes.minus(BigInt.fromI32(1));
       if (oldNodeTimezone.totalRegisteredNodes < BigInt.fromI32(0)) {
-        oldNodeTimezone.totalRegisteredNodes = BigInt.fromI32(0)
+        oldNodeTimezone.totalRegisteredNodes = BigInt.fromI32(0);
       }
     }
   }
 
   // If the newly set timezone doesn't exist yet, we need to create it.
-  let newNodeTimezoneId = getNodeTimezoneId(
-    event.params.node.toHex(),
-    event.address,
-  )
-  let newNodeTimezone: NetworkNodeTimezone | null = null
+  let newNodeTimezoneId = getNodeTimezoneId(event.params.node.toHex(), event.address);
+  let newNodeTimezone: NetworkNodeTimezone | null = null;
   if (newNodeTimezoneId != null) {
-    newNodeTimezone = NetworkNodeTimezone.load(newNodeTimezoneId)
+    newNodeTimezone = NetworkNodeTimezone.load(newNodeTimezoneId);
     if (newNodeTimezone === null || newNodeTimezone.id == null) {
-      newNodeTimezone = rocketPoolEntityFactory.createNodeTimezone(
-        newNodeTimezoneId,
-      )
+      newNodeTimezone = rocketPoolEntityFactory.createNodeTimezone(newNodeTimezoneId);
+      newNodeTimezone.block = event.block.number;
+      newNodeTimezone.blockTime = event.block.timestamp;
     }
 
     // Increment the total registered nodes for the new timezone and index.
-    newNodeTimezone.totalRegisteredNodes = newNodeTimezone.totalRegisteredNodes.plus(
-      BigInt.fromI32(1),
-    )
+    newNodeTimezone.totalRegisteredNodes = newNodeTimezone.totalRegisteredNodes.plus(BigInt.fromI32(1));
   }
 
-  if (oldNodeTimezone !== null) oldNodeTimezone.save()
-  if (newNodeTimezone !== null) newNodeTimezone.save()
+  if (oldNodeTimezone !== null) oldNodeTimezone.save();
+  if (newNodeTimezone !== null) newNodeTimezone.save();
 }
 
 /**
  * Returns the node timezone identifier based on what was in the smart contracts.
  */
-function getNodeTimezoneId(
-  nodeAddress: string,
-  nodeManagerContractAddress: Address,
-): string {
-  let rocketNodeManagerContract = rocketNodeManager.bind(
-    nodeManagerContractAddress,
-  )
-  let nodeTimezoneStringId = rocketNodeManagerContract.getNodeTimezoneLocation(
-    Address.fromString(nodeAddress),
-  )
-  if (nodeTimezoneStringId == null) nodeTimezoneStringId = 'UNKNOWN'
-  return nodeTimezoneStringId
+function getNodeTimezoneId(nodeAddress: string, nodeManagerContractAddress: Address): string {
+  let rocketNodeManagerContract = rocketNodeManager.bind(nodeManagerContractAddress);
+  let nodeTimezoneStringId = rocketNodeManagerContract.getNodeTimezoneLocation(Address.fromString(nodeAddress));
+  if (nodeTimezoneStringId == null) nodeTimezoneStringId = "UNKNOWN";
+  return nodeTimezoneStringId;
 }
